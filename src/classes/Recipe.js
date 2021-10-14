70847fc8615872421611d5c2341d89378fe7f136
Oscar Cortez <oacortez98@gmail.com>
2021-10-14 17:32:10 -0600

    Create properties for Recipe class

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,5 +1,13 @@
 class Recipe {
   constructor(recipeInfo) {
-    
+    this.id = recipeInfo.id;
+    this.image = recipeInfo.image;
+    this.ingredients = recipeInfo.ingredients;
+    this.instructors = recipeInfo.instructors;
+    this.name = recipeInfo.name;
+    this.tags = recipeInfo.tags;
+    this.ingredientsCost = 0;
   }
+
+  
 }
