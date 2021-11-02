f9b7972fe5b6ad97cc5901665825c9ee86d6b720
Oscar Cortez <oacortez98@gmail.com>
2021-11-02 15:10:26 -0600

    Updated file with new dummy data imports

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,75 +1,10 @@
 import { expect } from 'chai';
 import Recipe from '../src/classes/Recipe.js';
-// import ingredientsData from '../src/data/ingredients';
-
-const ingredientsData = [
-  {
-  "id": 20081,
-  "name": "wheat flour",
-  "estimatedCostInCents": 142
-},
-  {
-    "id": 93740,
-    "name": "blanched almond flour",
-    "estimatedCostInCents": 986
-  },
-{
-  "id": 1123,
-  "name": "eggs",
-  "estimatedCostInCents": 472
-},
-{
-  "id": 19335,
-  "name": "sucrose",
-  "estimatedCostInCents": 902
-},
-{
-  "id": 19206,
-  "name": "instant vanilla pudding",
-  "estimatedCostInCents": 660
-  }
-]
+import smallIngredientsData from '../src/data/dummyData/dummyIngredients';
+import smallRecipeData from '../src/data/dummyData/dummyRecipe'
 
 
-const recipeData = {
-  "id": 541288,
-"image": "https://spoonacular.com/recipeImages/541288-556x370.jpg",
-"ingredients": [
-  {
-    "id": 20081,
-    "quantity": {
-      "amount": 160,
-      "unit": "g"
-    }
-  },
-  {
-    "id": 93740,
-    "quantity": {
-      "amount": 40,
-      "unit": "g"
-    }
-  }
-],
-"instructions": [
-  {
-    "instruction": "Cut the butter into small cubes and keep them refrigerated until ready to use (I cut on parchment paper and wrap up the butter for easy transfer.).In the food processor, combine the flour, almond meal, sugar, and salt. If you don’t have a food processor, you can simply use a bowl to mix all the ingredients.If you want your sesame seeds to be fine texture, add them now. If you prefer to keep the original shape of sesame seeds, add them with egg yolk later on.Take out the butter from the refrigerator and mix together. If you use a regular bowl to mix, use a dough/pastry blender to combine the butter into the dry ingredients.Lastly add egg yolk.If the food processor is small (like mine) and it doesn’t look like it’s mixed completely, take it out and mix well with a silicone spatula.Form the dough into a ball and cut in half.",
-    "number": 1
-  },
-  {
-    "instruction": "Roll it to a log approximately 2” across. For me it’s easier to work when the dough is wrapped in plastic wrap. While rolling, unwrap some parts of plastic wrap then roll again. Form a nice shape. I wasn't paying attention so my log is flat on one side (see step 11)!Wrap the logs tightly in plastic wrap and refrigerate until firm, about 1 hour.Preheat the oven to 350° F (175° C).",
-    "number": 2
-  }
-],
-"name": "Sesame Cookies",
-"tags": [
-  "antipasti",
-  "starter",
-  "snack",
-  "appetizer",
-  "antipasto",
-  "hor d'oeuvre"
-  ]
-}
+const recipeData = smallRecipeData;
 
 describe('Recipe', () => {
 
@@ -103,7 +38,7 @@ beforeEach(() => {
     /** Todo : 
      * 
      */
-    const ingredients = recipe.getIngredientNames(ingredientsData)
+    const ingredients = recipe.getIngredientNames(smallIngredientsData)
     const answer = ["wheat flour", "blanched almond flour"];
 
     expect(ingredients).to.deep.equal(answer);
